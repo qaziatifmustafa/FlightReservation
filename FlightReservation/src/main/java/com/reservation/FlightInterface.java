8d09fd77357f9263d9817b6c02087ced3e6345b1
qaziatifmustafa <46591622+qaziatifmustafa@users.noreply.github.com>
2019-01-11 17:25:17 +0500

    Delete FlightInterface.java

--- FlightReservation/src/main/java/com/reservation/FlightInterface.java
+++
@@ -1,7 +0,0 @@
-package com.reservation;
-
-public interface FlightInterface {
-	/*public List<HashMap> businessFlightList= new ArrayList<HashMap>();
-	List<HashMap> getAllAvailableFlight();
-	void setAllAvailableFlight(ArrayList<HashMap> map);*/
-}
